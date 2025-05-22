1851e08185b2a1cc2e2968635f0044056b8606ec
minzoo <aw12813@gmail.com>
2025-05-22 17:38:34 +0900

    refactor: 불필요한 회원가입 기능 제거

--- app/admin/login/actions.ts
+++ app/admin/login/actions.ts
@@ -24,34 +24,3 @@ export async function login(formData: FormData) {
   revalidatePath("/", "layout");
   redirect("/admin");
 }
-
-export async function signup(formData: FormData) {
-  const supabase = await createClient();
-
-  const name = formData.get("name") as string;
-  const email = formData.get("email") as string;
-  const password = formData.get("password") as string;
-  const confirmPassword = formData.get("confirmPassword") as string;
-
-  // 비밀번호 확인
-  if (password !== confirmPassword) {
-    throw new Error("비밀번호가 일치하지 않습니다.");
-  }
-
-  const { error } = await supabase.auth.signUp({
-    email,
-    password,
-    options: {
-      emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/admin/auth/callback`,
-      data: {
-        user_name: name,
-      },
-    },
-  });
-
-  if (error) {
-    throw new Error(error.message);
-  }
-
-  revalidatePath("/", "layout");
-}
