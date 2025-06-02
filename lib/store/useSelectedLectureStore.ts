238324986288d4f8193a2750c37c319a76272aeb
wynter24 <m.wynter24.k@gmail.com>
2025-06-02 16:47:04 +0900

    feat: 서버 시간 API 추가 및 오픈된 과제만 제출 기능 활성화 #69

--- lib/store/useSelectedLectureStore.ts
+++ lib/store/useSelectedLectureStore.ts
@@ -4,14 +4,17 @@ import { create } from 'zustand';
 interface SelectedLectureState {
   lectureId: number;
   challengeLectureId: number;
+  open_at: string;
   setSelectedLecture: (selectedLecture: Lecture) => void;
 }
 
 export const useSelectedLectureStore = create<SelectedLectureState>(set => ({
   lectureId: 0,
   challengeLectureId: 0,
+  open_at: "",
   setSelectedLecture: (selectedLecture: Lecture) => set({
     lectureId: selectedLecture.id,
     challengeLectureId: selectedLecture.challenge_lecture_id,
+    open_at: selectedLecture.open_at,
   })
 }))
