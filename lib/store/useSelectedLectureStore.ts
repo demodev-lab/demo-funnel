5a3efd7783b63fe20907c5e475bb1c0a0bb016e0
wynter24 <m.wynter24.k@gmail.com>
2025-06-17 16:20:50 +0900

    refactor: getUserLectures api 호출 제거 및 getLecturesByChallenge 변경된 응답값 사용 #160

--- lib/store/useSelectedLectureStore.ts
+++ lib/store/useSelectedLectureStore.ts
@@ -1,18 +1,18 @@
-import { Lecture } from '@/types/lecture';
+import { LectureWithSequence } from '@/types/lecture';
 import { create } from 'zustand';
 
 interface SelectedLectureState {
   lectureId: number;
   challengeLectureId: number;
   open_at: string;
-  setSelectedLecture: (selectedLecture: Lecture) => void;
+  setSelectedLecture: (selectedLecture: LectureWithSequence) => void;
 }
 
 export const useSelectedLectureStore = create<SelectedLectureState>(set => ({
   lectureId: 0,
   challengeLectureId: 0,
   open_at: "",
-  setSelectedLecture: (selectedLecture: Lecture) => set({
+  setSelectedLecture: (selectedLecture: LectureWithSequence) => set({
     lectureId: selectedLecture.id,
     challengeLectureId: selectedLecture.challenge_lecture_id,
     open_at: selectedLecture.open_at,
