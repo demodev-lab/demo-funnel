20ccb9bf04c9fbe011c1f3bd0a7b4d0b0376843c
minzoo <aw12813@gmail.com>
2025-06-19 13:00:32 +0900

    refactor: challenge 인터페이스 프로퍼티명 camelCase로 수정 및 구조 수정

--- types/challenge.ts
+++ types/challenge.ts
@@ -2,31 +2,26 @@ export interface Challenge {
   id: number;
   name: string;
   description?: string;
-  start_date?: string;
-  end_date?: string;
-  created_at?: string;
-  updated_at?: string;
-  open_date: string;
-  close_date: string;
-  lecture_num: number;
-  startDate?: Date;
-  endDate?: Date;
-  lectureCount?: number;
+  createdAt?: string;
+  updatedAt?: string;
+  openDate: string;
+  closeDate: string;
+  lectureNum: number;
 }
 
 export type ChallengeFormData = Pick<
   Challenge,
-  "name" | "open_date" | "close_date" | "lecture_num"
+  "name" | "openDate" | "closeDate" | "lectureNum"
 >;
 
 export type UserChallenges = Pick<Challenge, "id" | "name">;
 
 export interface ChallengeUser {
-  challenge_id: number;
+  challengeId: number;
   Challenges: {
     id: number;
-    open_date: string;
-    close_date: string;
+    openDate: string;
+    closeDate: string;
   };
 }
 
