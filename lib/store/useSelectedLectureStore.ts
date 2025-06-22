3ad8e06e78f091fde85797ab7eb3e9d683ed6975
minzoo <aw12813@gmail.com>
2025-06-22 21:21:05 +0900

    fix: selectedLecture가 undefined일 때 발생하는 런타임 에러 수정

--- lib/store/useSelectedLectureStore.ts
+++ lib/store/useSelectedLectureStore.ts
@@ -1,20 +1,35 @@
-import { LectureWithSequence } from '@/types/lecture';
-import { create } from 'zustand';
+import { LectureWithSequence } from "@/types/lecture";
+import { create } from "zustand";
 
 interface SelectedLectureState {
   lectureId: number;
   challengeLectureId: number;
   open_at: string;
-  setSelectedLecture: (selectedLecture: LectureWithSequence) => void;
+  setSelectedLecture: (
+    selectedLecture: LectureWithSequence | null | undefined,
+  ) => void;
 }
 
-export const useSelectedLectureStore = create<SelectedLectureState>(set => ({
+export const useSelectedLectureStore = create<SelectedLectureState>((set) => ({
   lectureId: 0,
   challengeLectureId: 0,
   open_at: "",
-  setSelectedLecture: (selectedLecture: LectureWithSequence) => set({
-    lectureId: selectedLecture.id,
-    challengeLectureId: selectedLecture.challenge_lecture_id,
-    open_at: selectedLecture.open_at,
-  })
-}))
+  setSelectedLecture: (
+    selectedLecture: LectureWithSequence | null | undefined,
+  ) => {
+    if (!selectedLecture) {
+      set({
+        lectureId: 0,
+        challengeLectureId: 0,
+        open_at: "",
+      });
+      return;
+    }
+
+    set({
+      lectureId: selectedLecture.id,
+      challengeLectureId: selectedLecture.challenge_lecture_id,
+      open_at: selectedLecture.open_at,
+    });
+  },
+}));
